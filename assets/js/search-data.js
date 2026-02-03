3b68a88893554f4dbe4ece169f1edbf8419cd0c4
KostasBanos <banosk@mcmaster.ca>
2026-02-03 10:07:52 +0000

    Deploying to gh-pages from @ KostasBanos/KostasBanos.github.io@349210525e017b83492a2a45f5b70e3db90af274 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,18 +44,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/hobbies/";
           },
-        },{id: "post-coming-soon",
-        
-          title: "Coming soon",
-        
-        description: "Placeholder post.",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2026/empty-post/";
-          
-        },
-      },{id: "post-advanced-statistical-modeling-diagnostics-and-simulations",
+        },{id: "post-advanced-statistical-modeling-diagnostics-and-simulations",
         
           title: 'Advanced Statistical Modeling, Diagnostics and Simulations <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         
